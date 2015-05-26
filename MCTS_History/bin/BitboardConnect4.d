0e5b6ccb1ddcffa1c006ddfc60cf151d7dec6d9e
ildyria <beviguier@gmail.com>
2015-05-26 16:41:29 +0200

    trashes...

--- MCTS_History/bin/BitboardConnect4.d
+++
@@ -1,7 +0,0 @@
-bin/BitboardConnect4.o: src/connect4/BitboardConnect4.cpp \
- src/connect4/BitboardConnect4.h src/interfaces/Bitboard.h \
- src/tools/Count.h
-src/connect4/BitboardConnect4.cpp:
-src/connect4/BitboardConnect4.h:
-src/interfaces/Bitboard.h:
-src/tools/Count.h:
--- MCTS_History/bin/Node.d
+++
@@ -1,7 +0,0 @@
-bin/Node.o: src/mcts/Node.cpp src/mcts/Node.h src/interfaces/Bitboard.h \
- src/tools/Count.h src/interfaces/Move.h
-src/mcts/Node.cpp:
-src/mcts/Node.h:
-src/interfaces/Bitboard.h:
-src/tools/Count.h:
-src/interfaces/Move.h:
